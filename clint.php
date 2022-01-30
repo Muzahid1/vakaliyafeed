186cf456014e0941f06932fcd6ccc553d861512c
Muzahid1 <95116338+Muzahid1@users.noreply.github.com>
2022-01-30 09:01:14 +0530

    Add files via upload

---
+++ clint.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+  <Head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php
+    Include 'Header.Php';
+    ?>
+    <Style>
+  /* .nav{
+        Padding:0;
+        Margin:0;
+        Position:Fixed;
+        Box-sizing:Border-box;
+        Background:Blue;
+        Height:6%;
+        Width:100%;
+    } */
+
+        .cname{
+            Color:Green;
+            Border:Solid;
+            BoRder-radius:12px;
+            Border-color:Gray;
+           Height:25px;
+           Background:white;
+           Padding:15px;,25px;
+           Box-shadow: 3pX 2px 4pX #999;
+            
+        }
+   .Amain{
+        Padding:55px;
+        Margin-top:10px;
+        
+    }
+        .niche{
+            Margin-Left:0;
+            Right:0;
+            Margin-top:30px;
+            Margin-Bottom:0px;
+            Padding:25px;
+            Height:35px;
+            Background:#A9A9A9;
+            Bottom:0;
+
+        }
+        .img{
+            Margin:0;
+            Padding:0;
+            Border:soliD;
+            Border-color:White;
+            Border-radius:12px;
+            Box-shadow:-1Px 1px 3px 3Px #999;
+            Height:200px;
+        Width:150px;
+            Margin-left:90px;
+            Margin-right:80px;
+            
+            
+        }
+        .button {
+  background-color: #4CAF50; /* Green */
+  border: 1pX Solid #fff;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  display: Center;
+  font-size: 20px;
+  Margin:auto;
+  Width:60%;
+  Padding:20px;
+  Border-color:White;
+  Border-radius:7px;
+  Box-shadow:2px 3px 3Px #A9A9A9;
+  
+  
+}
+    </Style>
+<!--<Div Class="nav">
+<Nav Class="nav1"> Vakaliya Feed</nav><Nav Class="nav2"> Login </Nav></div> -->
+<Body>  
+<Div Class="Amain">
+    <H3 Class="cname">Godrej PoultryFeed</H3>
+        <H3 Class="cname"> Jafa Poultry Feed</H3></div>
+        <Img Class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/excel-1.jpg">
+        <Img Class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/excel-2.jpg">        
+        <Img Class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/excel-3.jpg">
+        <Img Class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/crumbro-1.jpg">
+        <Img Class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/crumbro-2.jpg">
+        <img class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/crumchick.jpg"   >
+          <img class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/crumgrow.jpg"   >
+    <img class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/crumegg1.jpg" >
+     <img class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/crumegg2.jpg" >
+      <img class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/crumegg3.jpg"   >
+          <img class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/eggy-1.jpg"   >
+    <img class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/eggy-2.jpg" >
+     <img class="img" Src="https://www.godrejagrovet.com/public/images/businesses/animal-feed/poultry-feed/eggy-3.jpg" >
+        <P Class="cname">Jafa Poultry Feed Only Phase 1 and Phase 2 available</P>
+        <P  Class="button"><A HreF="Tel:+919925985210">order Now </A></P><Br>
+       <P Class="button"> <?Php echo "Total Visitor"?> <?php echo date("i") ?></p>
+</Body>
+<Footer Class="niche">
+    Designed And Developed By <A Href="https://instagram.com/mr.muzu_04">Mh Vakaliya </a><Br>
+        ©Vakaliya <?php Echo Date('Y') ?> All Right Resirved
+
+</Footer>
---
+++ header.php
@@ -0,0 +1,35 @@
+<Doctype Html>
+    <Meta Charset="utf-8">
+        <Meta Name="viewport" Content="Width=device-width, Initial-Scale=1">
+     <Div Class="nav" > <Ul>
+     <Li Class="nav1"> Vakaliya Feed</li> <!--<Li>Login</li></ul>-->
+         </Div>
+         <Style>
+             .nav{
+                 Text-align:Center;
+                 Font-size:18px;
+                 Top:0;
+                 Left:;
+                 Right:0;
+                 Position:Fixed;
+                 Color:Blue;
+                 List-style-type:Square;
+                 Background-Color:White;
+                 Margin:0;
+                 Padding:0;
+                 Height:8%;
+                 Width:100%;
+                 Border:Solid 1pX White;
+                 Box-shadow:2px 2px 2px #999;
+                 
+             }
+             .nav Li{
+                 Float:Left;
+                 Text-align:Center;
+                 List-style:None;
+             }
+              .nav1{
+                  Text-align: Center;
+                  Display: Center;
+              }
+         </Style>
